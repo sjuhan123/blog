8e6986d665e54dc4faeb0ca580d28bff3e42a840
sjuhan123 <sjuhan123@gmail.com>
2024-03-13 00:00:31 +0900

    chore: install react, set config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  prefetch: false,
   integrations: [
     mdx(),
     sitemap(),
     tailwind({
       applyBaseStyles: false
     }),
+    react(),
     icon({
       include: {
         mdi: ['*'],
