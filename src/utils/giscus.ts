90f2e767c41f2e06cc0bb92d933af8bfe8efeb81
sjuhan123 <sjuhan123@gmail.com>
2024-03-14 14:53:37 +0900

    feat: giscus util

---
+++ src/utils/giscus.ts
@@ -0,0 +1,15 @@
+import { giscusThemes } from '../constants/giscus';
+
+export const changeGiscusTheme = (theme: keyof typeof giscusThemes) => {
+  const sendMessage = (config: unknown) => {
+    const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
+
+    iframe?.contentWindow?.postMessage({ giscus: config }, 'https://giscus.app');
+  };
+
+  sendMessage({
+    setConfig: {
+      theme: giscusThemes[theme]
+    }
+  });
+};
