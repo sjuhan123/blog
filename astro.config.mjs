b2cc0c2d8c429156a8968dc4b181af86ac4fd774
sjuhan123 <sjuhan123@gmail.com>
2024-03-19 14:57:55 +0900

    feat: mdx heading link

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://den-eight.vercel.app/',
   prefetch: false,
+  markdown: {
+    remarkPlugins: [[remarkToc, { heading: 'contents' }]],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap', properties: { className: 'anchor' } }]]
+  },
   integrations: [
     mdx(),
     sitemap(),
